fd2fc94ea241e796cebd646af08ce1f6d23395c3
German Rincon Urrego <>
2021-06-20 13:45:04 -0500

    Added base project.

---
+++ TestMVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestMVVM
+//
+//  Created by German Rincon Urrego on 20/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
